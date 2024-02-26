56046cd92fb2025bf94b3d444bf6f3c5a2b56f9b
MacbethCarrillo <31285010+MacbethCarrillo@users.noreply.github.com>
2024-02-26 16:10:40 -0700

    Delete Catalogo Equipos de Computo Con APP en Java directory

--- Catalogo Equipos de Computo Con APP en Java/Equipos.java
+++
@@ -1,9 +0,0 @@
-
-public class Equipos {
-	
-	public static void main(String[] args) {
-		Vista miVista = new Vista();
-		Controlador miControlador = new Controlador( miVista );
-		miControlador.iniciarVista();
-	}
-}
